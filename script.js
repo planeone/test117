8e0b4594d50f44b2eba3f36e369ff85c12d8bd65
planeone <91733615+planeone@users.noreply.github.com>
2021-11-04 16:41:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function drawGame(){
+	const scene=new THREE.Scene();
+	const camera=new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000);
+	
+	const render=new THREE.WebGLRenderer();
+	
+	render.setSize(window.innerWidth,window.innerHeight);
+	
+	document.body.prepend(render.domElement);
+	render.domElement.id="gameCanvas";
+	
+	let texture=new THREE.TextureLoader();
+	
+	scene.background=texture.load('./nebo.jpg');
+	
+	window.addEventListener("resize",function(){
+		render.setSize(window.innerWidth,window.innerHeight);
+		camera.aspect=window.innerWidth/window.innerHeight;
+		camera.updateProjectionMatrix();
+	});
+	function cycle(){
+		requestAnimationFrame(cycle);
+		render.render(scene,camera);
+	}
+	cycle();
+}
+const button=document.getElementById("start");
+button.addEventListener("mousedown",function(){
+	button.style.background="#00ff00";
+});
+button.addEventListener("mouseup",function(){
+	button.style.background="url(\"imageButton.png\")";
+	button.remove();
+	drawGame();
+});
