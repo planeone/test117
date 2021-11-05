2ec76afa858644e93f8ae700b159479b5069c05f
planeone <91733615+planeone@users.noreply.github.com>
2021-11-05 17:58:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function drawGame(){
 	
 	scene.background=texture.load('./nebo.jpg');
 	
-	const plane=new THREE.PlaneGeometry(1000,1000);
+	const plane=new THREE.PlaneGeometry(2000,1000);
 	const material=new THREE.MeshBasicMaterial({map:texture.load("grass.png")});
 	
 	const mesh=new THREE.Mesh(plane,material);
