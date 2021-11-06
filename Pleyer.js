32bd53b7d5ed6739485dda2a0d6d86c6cac7a6e6
planeone <91733615+planeone@users.noreply.github.com>
2021-11-06 16:50:44 +0200

    Update Pleyer.js

--- Pleyer.js
+++ Pleyer.js
@@ -3,14 +3,16 @@ class Pleyer{
 		this.scene=options.scene;
 		this.objLoade=new THREE.OBJLoader();
 		this.pos=options.pos;
-		this.rotate=options.rotate
+		this.rotate=options.rotate;
+		this.scale=options.scale;
 		this.car=options.car;
 		this.robot=options.robot;
 	}
-	draw(file,pos,rotation){
+	draw(file,pos,rotation,scale){
 		this.objLoade.load(this.car,(obj)=>{
 			obj.position.set(pos.x,pos.y,pos.z);
 			obj.rotation.set(rotation.x,rotation.y,rotation.z);
+			obj.scale.set(scale.x,scale.y,scale.z);
 			this.scene.add(obj);
 		});
 	}
