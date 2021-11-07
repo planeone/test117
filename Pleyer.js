812b0fa0927f529050c1a27ca3b6bf5dc703b5be
planeone <91733615+planeone@users.noreply.github.com>
2021-11-07 20:59:59 +0200

    Update Pleyer.js

--- Pleyer.js
+++ Pleyer.js
@@ -14,7 +14,7 @@ class Pleyer{
 			obj.position.set(pos.x,pos.y,pos.z);
 			obj.rotation.set(rotation.x,rotation.y,rotation.z);
 			obj.scale.set(scale.x,scale.y,scale.z);
-			obj.traverse(function(child){
+			obj.traverse((child)=>{
 				if(child instanceof THREE.Mesh){
 					child.material=this.materialCar[0];
 				}
