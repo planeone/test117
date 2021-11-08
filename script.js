7f0da28e7528c2eabd3ff1dfeef1a5d3af326c30
planeone <91733615+planeone@users.noreply.github.com>
2021-11-08 18:46:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -65,8 +65,8 @@ function drawGame(){
 		camera.updateProjectionMatrix();
 	});
 	function cycle(){
-		mesh.position.y+=10;
-		mesh2.position.y+=10;
+		mesh.position.y+=1;
+		mesh2.position.y+=1;
 		requestAnimationFrame(cycle);
 		render.render(scene,camera);
 	}
